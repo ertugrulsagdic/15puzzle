ff004ff963a67ef1693a6e2631cb185341888b4c
Göksel Tokur <gokseltokur@gmail.com>
2020-12-27 17:43:14 +0300

    fix bug random generator

--- random_puzzle_generator.py
+++ random_puzzle_generator.py
@@ -7,21 +7,23 @@ def generate_random_instance_of_puzzle(goal_puzzle, depth):
     check = []
 
     current_puzzle = goal_puzzle
-    
+    check.append(current_puzzle.puzzle)
     for i in range(depth):
         actions = current_puzzle.get_possible_actions()
         random_action = random.choice(actions)
+
         if random_action[0].puzzle in check:
             i -= 1
-        else:
-            current_puzzle = random_action[0]
-            check.append(random_action[0].puzzle)
+            continue
+
+        current_puzzle = random_action[0]
+        check.append(random_action[0].puzzle)
         
     return current_puzzle
 
+def test_random_generator():
+    goal_puzzle = Puzzle15(puzzle=[[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]], goal_state=[[1, 2, 3, 4], [12, 13, 14, 5], [11, 0, 15, 6], [10, 9, 8, 7]])
 
-goal_puzzle = Puzzle15(puzzle=[[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]], goal_state=[[1, 2, 3, 4], [12, 13, 14, 5], [11, 0, 15, 6], [10, 9, 8, 7]])
-
-print(generate_random_instance_of_puzzle(goal_puzzle, 2).render())
+    print(generate_random_instance_of_puzzle(goal_puzzle, 10).render())
         
 
